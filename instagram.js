fbafa1c4a7ca2a18871f5b0f8363e6874d165a37
Reza Fajri Fikri <rezafajrifikri@gmail.com>
2025-05-08 21:37:11 +0700

    Update instagram.js

--- instagram.js
+++ instagram.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Instagram Script (Optimized)
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Nyaa
 // @author       rezaffikri
 // @match        https://www.instagram.com/*
@@ -18,59 +18,72 @@
   let exploreNavElement = null;
   let targetLinkElement = null;
   let feedsRemoved = false;
+  let isExplorePage = window.location.href === 'https://www.instagram.com/explore/';
 
   const modifyUIElements = () => {
-    if (!reelsButton) {
-      reelsButton = document.querySelector('a[href="/reels/"]');
-    }
-    if (reelsButton) {
-      reelsButton.style.display = 'none';
-    } else {
-      console.warn('Reels button not found.');
+    try {
+      if (!reelsButton) {
+        reelsButton = document.querySelector('a[href="/reels/"]');
+      }
+      if (reelsButton) {
+        reelsButton.style.display = 'none';
+      } else {
+        console.warn('Reels button not found.');
+      }
+    } catch (error) {
+      console.error('Error in modifyUIElements:', error);
     }
   };
 
   const hideInstagramLitePrompt = () => {
-    if (instagramLitePromptButtons.length === 0) {
-      instagramLitePromptButtons = document.querySelectorAll('button');
-    }
-    instagramLitePromptButtons.forEach((button) => {
-      if (button.innerText.trim() === 'Use Instagram Lite') {
-        let parent = button;
-        for (let i = 0; i < 3; i++) {
-          parent = parent.parentElement;
-          if (!parent) break;
-        }
-        if (parent) {
-          parent.style.display = 'none';
-        } else {
-          console.warn('Instagram Lite prompt container not found.');
-        }
+    try {
+      if (instagramLitePromptButtons.length === 0) {
+        instagramLitePromptButtons = document.querySelectorAll('button');
       }
-    });
+      instagramLitePromptButtons.forEach((button) => {
+        if (button.innerText.trim() === 'Use Instagram Lite') {
+          let parent = button;
+          for (let i = 0; i < 3; i++) {
+            parent = parent.parentElement;
+            if (!parent) break;
+          }
+          if (parent) {
+            parent.style.display = 'none';
+          } else {
+            console.warn('Instagram Lite prompt container not found.');
+          }
+        }
+      });
+    } catch (error) {
+      console.error('Error in hideInstagramLitePrompt:', error);
+    }
   };
 
   const hideExploreSection = () => {
-    if (window.location.href === 'https://www.instagram.com/explore/') {
-      if (!exploreNavElement) {
-        exploreNavElement = document.querySelector('nav');
-      }
-      if (exploreNavElement) {
-        const nextSibling = exploreNavElement.nextElementSibling;
-        if (nextSibling && nextSibling.nodeName === 'DIV') {
-          const targetDiv = nextSibling.querySelector('div > div > div');
-          if (targetDiv) {
-            targetDiv.style.display = 'none';
-          } else {
-            console.warn('Could not find the target nested div on Explore page.');
-          }
-        } else if (nextSibling) {
-          console.warn('Found nav, but the next sibling is not a div on Explore page.');
-        } else {
-          console.warn('Found nav, but there is no next sibling on Explore page.');
+    if (isExplorePage) {
+      try {
+        if (!exploreNavElement) {
+          exploreNavElement = document.querySelector('nav');
         }
-      } else {
-        console.warn('Could not find any nav element on the Explore page.');
+        if (exploreNavElement) {
+          const nextSibling = exploreNavElement.nextElementSibling;
+          if (nextSibling && nextSibling.nodeName === 'DIV') {
+            const targetDiv = nextSibling.querySelector('div > div > div');
+            if (targetDiv) {
+              targetDiv.style.display = 'none';
+            } else {
+              console.warn('Could not find the target nested div on Explore page.');
+            }
+          } else if (nextSibling) {
+            console.warn('Found nav, but the next sibling is not a div on Explore page.');
+          } else {
+            console.warn('Found nav, but there is no next sibling on Explore page.');
+          }
+        } else {
+          console.warn('Could not find any nav element on the Explore page.');
+        }
+      } catch (error) {
+        console.error('Error in hideExploreSection:', error);
       }
     }
   };
@@ -88,22 +101,28 @@
   };
 
   function removeSponsored() {
-    const articles = document.querySelectorAll('article');
+    try {
+      if (window.location.href === 'https://www.instagram.com/') {
+        const articles = document.querySelectorAll('article');
 
-    articles.forEach((article) => {
-      const spans = article.querySelectorAll('span');
-      let isSponsored = false;
+        articles.forEach((article) => {
+          const spans = article.querySelectorAll('span');
+          let isSponsored = false;
 
-      spans.forEach((span) => {
-        if (span.textContent.toLowerCase().includes('sponsored')) {
-          isSponsored = true;
-        }
-      });
+          spans.forEach((span) => {
+            if (span.textContent.toLowerCase().includes('sponsored')) {
+              isSponsored = true;
+            }
+          });
 
-      if (isSponsored) {
-        article.style.display = 'none';
+          if (isSponsored) {
+            article.style.display = 'none';
+          }
+        });
       }
-    });
+    } catch (error) {
+      console.error('Error in removeSponsored', error);
+    }
   }
 
   let lastScrollTop = 0;
@@ -111,27 +130,31 @@
   let targetLinkPosition = 0;
 
   function observeScrollStopLink() {
-    if (!targetLinkElement) {
-      targetLinkElement = document.querySelector('a[href*="?variant=past_posts"]');
-    }
+    try {
+      if (!targetLinkElement) {
+        targetLinkElement = document.querySelector('a[href*="?variant=past_posts"]');
+      }
 
-    if (!targetLinkElement) {
-      setTimeout(observeScrollStopLink, 1000);
-      return;
-    }
+      if (!targetLinkElement) {
+        setTimeout(observeScrollStopLink, 1000);
+        return;
+      }
 
-    targetLinkPosition = targetLinkElement.getBoundingClientRect().top + window.scrollY;
+      targetLinkPosition = targetLinkElement.getBoundingClientRect().top + window.scrollY;
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          scrollLocked = true;
-          observer.disconnect();
-        }
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            scrollLocked = true;
+            observer.disconnect();
+          }
+        });
       });
-    });
 
-    observer.observe(targetLinkElement);
+      observer.observe(targetLinkElement);
+    } catch (error) {
+      console.error('Error in observeScrollStopLink:', error);
+    }
   }
 
   window.addEventListener(
@@ -155,21 +178,34 @@
   );
 
   const observer = new MutationObserver(() => {
+    // Check if the body has been replaced, and re-initialize if necessary
+    if (document.body) {
+      reinitialize(); // Call reinitialize instead of calling functions individually
+    }
+  });
+
+  function reinitialize() {
+    //Re-initialize
+    reelsButton = null;
+    instagramLitePromptButtons = [];
+    exploreNavElement = null;
+    targetLinkElement = null;
+    feedsRemoved = false;
+    isExplorePage = window.location.href === 'https://www.instagram.com/explore/';
+
     modifyUIElements();
     hideInstagramLitePrompt();
     checkAndRedirect();
     hideExploreSection();
     removeSponsored();
     observeScrollStopLink();
-  });
+  }
+
+  // Initializations and running outside the observer, to run once on page load
+  reinitialize();
 
   observer.observe(document.body, {
     childList: true,
     subtree: true,
   });
-
-  // Run these outside the observer, to run once on page load
-  modifyUIElements();
-  hideExploreSection();
-  checkAndRedirect();
 })();
